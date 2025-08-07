c1f5c5426fae91f38793b29cb1e9c622e99d4e6b
Yehan Raveesha <yrlwijesekara@students.nsbm.ac.lk>
2025-08-07 11:30:59 +0530

    add Leaflet and React-Leaflet dependencies; remove unused MapComponent.css; import Leaflet CSS in MapComponent

--- frontend/src/components/MapComponent.jsx
+++ frontend/src/components/MapComponent.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 // Fix for default markers in React-Leaflet
 delete L.Icon.Default.prototype._getIconUrl;
